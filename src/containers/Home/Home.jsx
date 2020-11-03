6fbed653a41e11c392c39dcff27cfac2cb5adc59
dagreatcode <dagreatcode@gmail.com>
2020-11-03 15:48:27 -0500

    fixed links and page routes

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Hello Home</h1>
+  return (
+    <div>
+      <div class="card">
+        <div class="card-body">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-5">
+                <div class="page-header">
+                  <h1>About Me</h1>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-3">
+                <img
+                  src="./assets/Images/img1.png"
+                  class="img-thumbnail"
+                  alt="..."
+                />
+              </div>
+              <div class="col-sm-3">
+                <div class="caption">
+                  <p>
+                    A Little About Me. Well i have been a creator forever.
+                    Music, anima, and now i want to make a living making
+                    websites. I have always looked at coding. I always wanted to
+                    know how it works. I started of learning python and it was
+                    really hard. That is what made me love it so much. The fact
+                    that i can do a little. I really want to find out where I
+                    end up and how far I go.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-8">
+                <div class="caption">
+                  <p>
+                    I have a son with autism. Its alot but I know god would
+                    never but too much on me i can not handle. Like This
+                    project. I have been on a long road to finding myself. I had
+                    to let alot of people go because they did not want anything,
+                    but i wanted everything. I have never gave up on anything no
+                    matter how bad it get, i still seem to want to do more. Its
+                    so easy to give up, and i never want anything easy. This
+                    project was hard but i still put my all into it... i stay up
+                    long nights trying to do it on my own. I have made alot of
+                    mistake. But i am still pushing forwards.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
 
 const Portfolio = () => {
-    return (
-        <div>
-            <h1>Hello Portfolio</h1>
+  return (
+    <div>
+      <div class="card" >
+        <div class="card-body">
+          <h3 class="card-title">Portfolio</h3>
+          <img
+            src="./assets/Images/img1.png"
+            class="rounded float-left"
+            alt="..."
+          />
+          <img
+            src="./assets/Images/img1.png"
+            class="rounded float-left"
+            alt="..."
+          />
+          <img
+            src="./assets/Images/img1.png"
+            class="rounded float-left"
+            alt="..."
+          />
+          <img
+            src="./assets/Images/img1.png"
+            class="rounded float-left"
+            alt="..."
+          />
+          <img
+            src="./assets/Images/img1.png"
+            class="rounded float-left"
+            alt="..."
+          />
+          <img
+            src="./assets/Images/img1.png"
+            class="rounded float-left"
+            alt="..."
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
