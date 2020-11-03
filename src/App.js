9a1b9a223215d9ac4d4ac56c2b07df1286d1f831
dagreatcode <dagreatcode@gmail.com>
2020-11-03 13:19:09 -0500

    testing the look of the links and got them working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Home from "./containers/Home/Home";
+import Portfolio from "./containers/Portfolio/Portfolio";
+import Contact from "./containers/Contact/Contact";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <div>
-        <Link to="/">Home</Link>
-      </div>
-      <div>
-        <Link to="Portfolio">Portfolio</Link>
-      </div>
-      <div>
-        <Link to="Contact">Contact</Link>
-      </div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+        <div>
+          <Link to="Portfolio">Portfolio</Link>
+        </div>
+        <div>
+          <Link to="Contact">Contact</Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
       </Router>
     </div>
   );
