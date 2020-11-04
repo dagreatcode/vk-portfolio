e46c626e9967f7098ed872ba2dc223a99a0dbde8
dagreatcode <dagreatcode@gmail.com>
2020-11-03 21:55:29 -0500

    everything is working

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -1,33 +1,17 @@
 import React from "react";
 // import img1 from "img/img1.png";
-
+import profileImage from "./img/img1.png";
 
 const Portfolio = () => {
   return (
     <div>
-      <div className="card" >
+      <div className="card">
         <div className="card-body">
           <h3 className="card-title">Portfolio</h3>
-          <img
-            src="{img1}"
-            className="rounded float-left"
-            alt="..."
-          />
-          <img
-            src="img1.png"
-            className="rounded float-left"
-            alt="..."
-          />
-          <img
-            src="/public/img1.png"
-            className="rounded float-left"
-            alt="..."
-          />
-          <img
-            src="./img1.png"
-            className="rounded float-left"
-            alt="..."
-          />
+          <img src={profileImage} className="img-thumbnail" alt="..." />
+          <img src={profileImage} className="rounded float-left" alt="..." />
+          <img src={profileImage} className="img-thumbnail" alt="..." />
+          <img src={profileImage} className="rounded float-left" alt="..." />
         </div>
       </div>
     </div>
