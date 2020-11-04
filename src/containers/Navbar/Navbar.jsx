21af4736365aed697f58d928ccda0987b2a4ca53
dagreatcode <dagreatcode@gmail.com>
2020-11-04 15:35:35 -0500

    got links to work

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Home from "../../containers/Home/Home";
+import Portfolio from "../../containers/Portfolio/Portfolio";
+import Contact from "../../containers/Contact/Contact";
 
 const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="/">Vincent Kendrick Portfolio</a>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
+                <Router>
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
                             {/* <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>     */}
@@ -21,10 +26,13 @@ const Navbar = () => {
                             <Link to="/contact">Contact</Link>
                         </li>
                     </ul>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                </div>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/portfolio" component={Portfolio} />
+                        <Route exact path="/contact" component={Contact} />
+                    </Switch>
+                </Router>
+                {/* </div> */}
             </nav>
         </div>
     );
