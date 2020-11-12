3b3676df66670e6f60e4a739c3c9984a71bf1a6e
dagreatcode <dagreatcode@gmail.com>
2020-11-12 00:32:18 -0500

    updated the contact

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -9,26 +9,42 @@ const Contact = () => {
       <h3>My Email: dagreatcode@gmail.com</h3>
 
       <div>
-      <a href="https://boiling-fortress-06417.herokuapp.com/index">
-        First Homework Link
+      <a href="https://github.com/dagreatcode">
+        My GitHub Page
       </a>
       </div>
+      <h4>Homework</h4>
       <div>
-        <a href="https://pacific-fortress-34769.herokuapp.com/">
-          Second Homework Link
-        </a>
+      <a href="https://pacific-fortress-34769.herokuapp.com/">Website</a>
+        <a href="https://github.com/dagreatcode/our-budget-tracker.git">Homework</a>
+    
       </div>
+      <h4>Project 1</h4>
       <div>
-        <a href="https://dagreatcode.github.io/project-2/">First Project</a>
+        <a href="https://dagreatcode.github.io/project-2/">Website</a>
+        <a href="https://github.com/dagreatcode/project-2">GitHub Project</a>
+        
+  
       </div>
-      <a href="https://boiling-fortress-06417.herokuapp.com/index">
-        Second Project
-      </a>
+      <h4>Project 2</h4>
       <div>
+        <a href="https://boiling-fortress-06417.herokuapp.com/index">Website Project</a>
+        <a href="https://github.com/dagreatcode/project-2-group-15.git">GitHub Project</a>
+      </div>
+      <h4>Homework</h4>
+      <div>
+        <a href="https://workoutwatcher.herokuapp.com/">Homework</a>
+        <a href="https://github.com/dagreatcode/Workout-Tracker.git">Github</a>
+      </div>
+
+   
+      <div>
+          <h4>LinkedIn</h4>
         <a href="https://www.linkedin.com/in/vincent-kendrick-baab171b2/">
           My Linkedin
         </a>
       </div>
+      <h4>Resume</h4>
       <a href="https://docs.google.com/document/d/1Ptx5x2S5qKHgHC2DALhAK5AOjyW5RFyhRwSoGi3RDbM/edit?usp=sharing">
         My Resume
       </a>
